a4ec47ddd02d8f2851c76fa064ef6939bddddd9f
Rajit Bhattacharya <rajitbh@gmail.com>
2018-09-13 13:10:04 +0530

    pyne

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Shreya Paul.", "Learner.", "Listener." , "Dreamer."],
+      strings: ["Shreya Paul.", "A Learner.", "A Listener." , "A Dreamer."],
       typeSpeed: 100,
       loop: true,
     });
